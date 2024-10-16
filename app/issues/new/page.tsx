63b58caddd03b747f58b8973d71cd9878d5b172f
Lolade Ilori <loladeilori02@gmail.com>
2024-10-16 17:24:01 +0100

    handled error

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
-import { Button, TextArea, TextField } from '@radix-ui/themes'
+import { Button, Callout, TextArea, TextField } from '@radix-ui/themes'
 import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 import axios from 'axios';
 import "easymde/dist/easymde.min.css";
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { BsInfoCircle } from 'react-icons/bs';
 
 interface IssueForm {
   title: string,
@@ -15,30 +17,50 @@ interface IssueForm {
 const NewIssue = () => {
   const router = useRouter()
   const { register, handleSubmit, control, formState: { errors } } = useForm<IssueForm>();
+
+  const [error, setError] = useState("")
  
   return (
-    <form onSubmit={handleSubmit(async (data) => {
-      await axios.post("/api/issues", data)
-      router.push("/issues")
-    })} 
-      className='max-w-xl space-y-3'
-    >
-        <TextField.Root>
-            <TextField.Input placeholder='Title' {...register("title", {required: true})} />
-        </TextField.Root>
-        {errors.title && <span>This field is required</span>}
 
-        <Controller 
-          name= "description"
-          control={control}
-          rules={{ required: true }}
-          render={({field}) => <SimpleMDE placeholder='Description' {...field}/>}
-        />
-        {errors.description && <span>This field is required</span>}
+    <div className='max-w-xl'>
+      {error && 
+        <Callout.Root color='red' className='mb-5'>
+          <Callout.Icon>
+            <BsInfoCircle />
+          </Callout.Icon>
+          <Callout.Text>
+            {error}
+          </Callout.Text>
+        </Callout.Root>
+      }
+      <form onSubmit={handleSubmit(async (data) => {
+        try {
+          await axios.post("/api/issues", data)
+          router.push("/issues")
+        } catch (error) {
+          setError("An unexpected error occured")
+        }
+      })} 
+        className='space-y-3'
+      >
+          <TextField.Root>
+              <TextField.Input placeholder='Title' {...register("title", {required: false})} />
+          </TextField.Root>
+          {errors.title && <span>This field is required</span>}
 
-        
-        <Button>Submit New Issue</Button>
-    </form>
+          <Controller 
+            name= "description"
+            control={control}
+            rules={{ required: true }}
+            render={({field}) => <SimpleMDE placeholder='Description' {...field}/>}
+          />
+          {errors.description && <span>This field is required</span>}
+
+          
+          <Button>Submit New Issue</Button>
+      </form>
+    </div>
+
   )
 }
 
