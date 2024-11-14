54bfc4835103539611e9ecd537f3c1e916994a25
Lolade Ilori <loladeilori02@gmail.com>
2024-11-14 13:40:29 +0000

    fixed the 500 error when creating new issue

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -3,7 +3,8 @@ import { z } from 'zod'
 // give the object that defines the shape of our user object, this helps us define our validation rules
 const issuesSchema = z.object({
     title: z.string().min(1, "Title is required").max(255),
-    description: z.string().min(1, "Description is required")
+    description: z.string().min(1, "Description is required").max(65535),
+    // assignedUserId: z.string().min(1, "AssignedUserId is required").max(255)
 })
 
 export default issuesSchema;
