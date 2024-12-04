c2dae3fdc41e2398ab5cb6713c6898fa84965f07
Lolade Ilori <loladeilori02@gmail.com>
2024-12-04 12:22:36 +0000

    sortingprisma xata issues

--- prisma/migrations/20241114133658_made_assigned_user_id_optional/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `Issue` DROP FOREIGN KEY `Issue_assignedUserId_fkey`;
-
--- AlterTable
-ALTER TABLE `Issue` MODIFY `assignedUserId` VARCHAR(255) NULL;
-
--- AddForeignKey
-ALTER TABLE `Issue` ADD CONSTRAINT `Issue_assignedUserId_fkey` FOREIGN KEY (`assignedUserId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
